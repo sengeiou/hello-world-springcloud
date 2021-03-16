006145efc1c7c515cf53f5498fb4ca9793f1ac2b
clh <cailonghao1234@foxmail.com>
2021-03-16 12:07:41 +0800

    no message

--- swan-user/src/main/java/com/user/swan/controller/UserController.java
+++ swan-user/src/main/java/com/user/swan/controller/UserController.java
@@ -6,10 +6,12 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+@RefreshScope
 @Slf4j
 @Data
 @RestController
@@ -18,6 +20,11 @@ public class UserController {
 
     @Value("${server.port}")
     private String host;
+    @Value("${config.server.user.name}")
+    private String name;
+    @Value("${config.server.user.port}")
+    private String port;
+
 
     @Autowired
     RibbonOrderService ribbonOrderService;
@@ -35,4 +42,14 @@ public class UserController {
         log.info("调用失败");
         return orderFeign.askIndex("hehhe");
     }
+
+    @RequestMapping("/config")
+    public String config() {
+        log.info("调用失败");
+        log.info("name={}", name);
+        log.info("port={}", port);
+        return name + port;
+    }
+
+
 }
